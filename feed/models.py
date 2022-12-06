cd19a0d7c359e4fb70eb3cea2e120c374e45cb87
taimoor954 <taimoormuhammad954@gmail.com>
2022-12-06 12:07:50 +0500

    added a created_at field in model

--- feed/models.py
+++ feed/models.py
@@ -3,8 +3,9 @@ from sorl.thumbnail import ImageField
 
 
 class Posts(models.Model):
-    text = models.CharField(max_length=160, blank=False, null=False)
+    text = models.CharField(max_length=170, blank=False, null=False)
     image = ImageField()
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.text
